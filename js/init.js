d7366697a11c361ead20ddc29cec9fe7943c8587
Adrix1 <adrixmalagon@gmail.com>
2017-03-20 00:11:09 -0600

    Se realizan primeros cambios en la página BLK con Materialize

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    // $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  });
+})(jQuery);
+
+  $(document).ready(function(){
+    $(".button-collapse").sideNav();
+ });
+
+$(document).ready(function(){
+      $('.carousel').carousel();
+    });
